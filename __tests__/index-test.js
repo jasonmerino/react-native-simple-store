9c5d9f49c9fd0b4200ef6282c3faf9c3af8853f8
Jason Merino <jasonmerino@gmail.com>
2016-08-24 16:46:00 -0700

    Adds the beginning of examples. (#17)

--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -2,22 +2,33 @@
 
 function getTestData() {
 	return {
-		isATest: true
+		isATest: true,
+		hasNestedData: {
+			ohYeah: 'it\s true',
+		}
 	};
 }
 
 function multiGetTestData() {
 	return [
-		['key1', JSON.stringify({valor: 1})],
-		['key2', JSON.stringify({valor: 2})]
+		['key1', JSON.stringify({ valor: 1 })],
+		['key2', JSON.stringify({ valor: 2 })],
+	];
+};
+
+function multiSaveTestData() {
+	return [
+		['key1', { valor: 1 }],
+		['key2', { valor: 2 }],
 	];
 };
 
 const INDEX_PATH = '../src/index';
 
 jest.unmock(INDEX_PATH);
+jest.unmock('lodash.merge');
 
-jest.setMock('react-native', {
+jest.mock('react-native', () => ({
 	AsyncStorage: {
 		setItem: jest.fn(() => {
 			return new Promise((resolve, reject) => {
@@ -50,80 +61,111 @@ jest.setMock('react-native', {
 			});
 		})
 	}
-});
+}));
 
-describe('save', function() {
 
-	it('should return a promise with no errors', function() {
-		var store = require(INDEX_PATH);
-		return store.save('testing', getTestData()).then(function(error) {
+describe('save', () => {
+
+	it('should return a promise with no errors', () => {
+		const { AsyncStorage } = require('react-native');
+		const store = require(INDEX_PATH);
+		return store.save('testing', getTestData()).then((error) => {
 			expect(error).toEqual(null);
+			expect(AsyncStorage.setItem).toBeCalledWith('testing', JSON.stringify(getTestData()));
 		});
 	});
 
-	it('should return a promise with no errors', function() {
-		var store = require(INDEX_PATH);
-		return store.save(multiGetTestData()).then(function(error) {
+	it('should return a promise with no errors', () => {
+		const { AsyncStorage } = require('react-native');
+		const store = require(INDEX_PATH);
+		const result = [
+			['key1', JSON.stringify({ valor: 1 })],
+			['key2', JSON.stringify({ valor: 2 })],
+		];
+		return store.save(multiSaveTestData()).then((error) => {
 			expect(error).toEqual(null);
+			expect(AsyncStorage.multiSet).toBeCalledWith(result);
 		});
 	});
 
 });
 
-describe('get', function() {
+describe('get', () => {
 
-	it('should return a promise with saved data', function() {
-		var store = require(INDEX_PATH);
-		return store.get('testing').then(function(error) {
+	it('should return a promise with saved data', () => {
+		const { AsyncStorage } = require('react-native');
+		const store = require(INDEX_PATH);
+		return store.get('testing').then((error) => {
 			expect(error).toEqual(getTestData());
+			expect(AsyncStorage.getItem).toBeCalledWith('testing');
 		});
 	});
 
-	it('should return a promise with saved data', function() {
-		var store = require(INDEX_PATH);
-		return store.get(['testing', 'testing']).then(function(error) {
+	it('should return a promise with saved data', () => {
+		const { AsyncStorage } = require('react-native');
+		const store = require(INDEX_PATH);
+		return store.get(['testing', 'testing']).then((error) => {
 			expect(error).toEqual([{valor: 1}, {valor: 2}]);
+			expect(AsyncStorage.multiGet).toBeCalledWith(['testing', 'testing']);
 		});
 	});
 
 });
 
-describe('update', function() {
+describe('update', () => {
 
-	pit('should return a promise with no errors', function() {
-		var store = require(INDEX_PATH);
+	it('should return a promise with no errors', () => {
+		const { AsyncStorage } = require('react-native');
+		const store = require(INDEX_PATH);
 		return store.update('testing', {
-			isAGoodTest: false
-		}).then(function(error) {
+			isAGoodTest: false,
+			hasNestedData: {
+				boom: true,
+			},
+		}).then((error) => {
 			expect(error).toEqual(null);
+			expect(AsyncStorage.setItem).toBeCalledWith('testing', JSON.stringify({
+				isATest: true,
+				hasNestedData: {
+					ohYeah: 'it\s true',
+					boom: true,
+				},
+				isAGoodTest: false,
+			}));
 		});
 	});
 
-	pit('should handle a string and return a promise with no errors', function() {
-		var store = require(INDEX_PATH);
-		return store.update('testing', 'asdf').then(function(error) {
+	it('should handle a string and return a promise with no errors', () => {
+		const { AsyncStorage } = require('react-native');
+		const store = require(INDEX_PATH);
+		return store.update('testing', 'asdf').then((error) => {
 			expect(error).toEqual(null);
+			expect(AsyncStorage.setItem).toBeCalledWith('testing', JSON.stringify('asdf'));
 		});
 	});
 
 });
 
-describe('delete', function() {
+describe('delete', () => {
 
-	pit('should return a promise with no errors', function() {
-		var store = require(INDEX_PATH);
-		return store.delete('testing').then(function(error) {
+	it('should return a promise with no errors', () => {
+		const { AsyncStorage } = require('react-native');
+		const store = require(INDEX_PATH);
+		return store.delete('testing').then((error) => {
 			expect(error).toEqual(null);
+			expect(AsyncStorage.removeItem).toBeCalledWith('testing');
 		});
 	});
 
 });
 
-describe('keys', function() {
+describe('keys', () => {
 	it('should return the keys', () => {
-		var store = require(INDEX_PATH);
+		const { AsyncStorage } = require('react-native');
+		const store = require(INDEX_PATH);
 		return store.keys().then((keys) => {
 			expect(keys).toEqual(['one', 'two', 'three']);
+			expect(AsyncStorage.getAllKeys).toBeCalled();
 		});
 	});
 });
